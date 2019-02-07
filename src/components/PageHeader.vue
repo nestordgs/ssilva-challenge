7face61f5daf77efca4647e2142265567e78bf84
Nestor Gutiérrez <nesor2@gmail.com>
2019-02-07 03:04:51 -0400

    Home, Persons & Films Page
    - Modified Home page.
    - Created Persons page.
    - Created Films page.
    - Created new page header component.
    - Created form search component.
    - Created Routes to persons & films.
    - Modified layout.

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="d-flex justify-content-around p-2 bg-dark">
+      <div class="p-2">
+        <router-link class="text-light" :to="{ name: 'home' }">
+          <font-awesome icon="home" /> Home
+        </router-link>
+      </div>
+      <div class="p-2">
+        <router-link class="text-light" :to="{ name: 'persons' }">
+          <font-awesome icon="users" /> Persons
+        </router-link>
+      </div>
+      <div class="p-2">
+        <router-link class="text-light" :to="{ name: 'films' }">
+          <font-awesome icon="film" /> Films
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/Film.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="person">
+    <h1>This is an Films page</h1>
+    <b-row>
+      <b-col>
+
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "films"
+};
+</script>
+
